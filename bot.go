30aa2f308410c3bb2218aa4c0cf96b3d47c89aef
Yaroslav Romanov <romanov@smartdec.ru>
2016-06-24 16:46:26 +0300

    Refactoring

--- bot.go
+++ bot.go
@@ -8,87 +8,94 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"io/ioutil"
 	"log"
-	"net/http"
+  "strconv"
 )
 
+type TwitterBot struct {
+  botApi     *tgbotapi.BotAPI
+  client *anaconda.TwitterApi
+}
+
+func New(token string, consumer_key string, consumer_secret string) *TwitterBot {
+  bot, err := tgbotapi.NewBotAPI(token)
+  if err != nil {
+    log.Panic(err)
+  }
+  bot.Debug = true
+  log.Printf("Authorized on account %s", bot.Self.UserName)
+	
+  anaconda.SetConsumerKey(consumer_key)
+	anaconda.SetConsumerSecret(consumer_secret)
+  
+  twitterBot := new(TwitterBot)
+  twitterBot.botApi = bot
+  twitterBot.client = nil
+  return twitterBot
+}
+
 type Config struct {
-	TelegramToken         string `json:"token"`
-	TwitterConsumerKey    string `json:"consumer_key"`
-	TwitterConsumerSecret string `json:"consumer_secret"`
+  TelegramToken         string `json:"token"`
+  TwitterConsumerKey    string `json:"consumer_key"`
+  TwitterConsumerSecret string `json:"consumer_secret"`
 }
 
 type Twitter struct {
-	Api       *anaconda.TwitterApi
-	TempCreds *oauth.Credentials
-	Verifier  string
-	URL       string
+  Api       *anaconda.TwitterApi
+  TempCreds *oauth.Credentials
+  Verifier  string
+  URL       string
 }
 
 var twitter Twitter
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	twitter.Verifier = r.URL.Query()["oauth_verifier"][0]
-
-	user_creds, _, err := anaconda.GetCredentials(twitter.TempCreds, twitter.Verifier)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	twitter.Api = anaconda.NewTwitterApi(user_creds.Token, user_creds.Secret)
-	http.Redirect(w, r, "https://www.twitter.com", 301)
-}
-
 func main() {
-	configPtr := flag.String("config", "./config/config.json", "Path to config file")
-	flag.Parse()
+  configPtr := flag.String("config", "./config/config.json", "Path to config file")
+  flag.Parse()
 
-	var config Config
-	array, err := ioutil.ReadFile(*configPtr)
-	if err != nil {
-		log.Panic(err)
-	}
-	json.Unmarshal(array, &config)
+  var config Config
+  array, err := ioutil.ReadFile(*configPtr)
+  if err != nil {
+    log.Panic(err)
+  }
+  json.Unmarshal(array, &config)
 
-	anaconda.SetConsumerKey(config.TwitterConsumerKey)
-	anaconda.SetConsumerSecret(config.TwitterConsumerSecret)
-	url, creds, err := anaconda.AuthorizationURL("http://127.0.0.1:8080/twitter_callback")
+  twitBot := New(config.TelegramToken, config.TwitterConsumerKey, config.TwitterConsumerSecret)
+
+	url, creds, err := anaconda.AuthorizationURL("oob")
 	if err != nil {
 		log.Panic(err)
 	}
 	twitter.URL = url
 	twitter.TempCreds = creds
 
-	http.HandleFunc("/twitter_callback", handler)
-	go http.ListenAndServe(":8080", nil)
-
-	bot, err := tgbotapi.NewBotAPI(config.TelegramToken)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	bot.Debug = true
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
 	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
 	ucfg.Timeout = 60
-	updates, err := bot.GetUpdatesChan(ucfg)
+	updates, err := twitBot.botApi.GetUpdatesChan(ucfg)
 
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
-		if update.Message.Text == "test_auth" {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, twitter.URL)
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
-			continue
-		}
+
+    if _, err := strconv.Atoi(update.Message.Text); err == nil && twitter.Api == nil {
+      user_creds, _, err := anaconda.GetCredentials(twitter.TempCreds, update.Message.Text)
+      if err != nil {
+        log.Panic(err)
+      }
+      if user_creds != nil {
+        twitter.Api = anaconda.NewTwitterApi(user_creds.Token, user_creds.Secret)
+        msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Successfuly authorized. Enjoy your time :)")
+        msg.ReplyToMessageID = update.Message.MessageID
+        twitBot.botApi.Send(msg)
+      }
+      continue
+    }
 
 		if twitter.Api == nil {
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You need to give access to twitter to use bot. Follow the link below:\r\n")
 			msg.Text += twitter.URL
 			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
+			twitBot.botApi.Send(msg)
 			continue
 		}
 
@@ -99,6 +106,6 @@ func main() {
 		for _, tweet := range searchResult.Statuses {
 			msg.Text += tweet.Text + "\r\n=======\r\n"
 		}
-		bot.Send(msg)
+		twitBot.botApi.Send(msg)
 	}
 }
